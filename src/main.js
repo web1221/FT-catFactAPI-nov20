c752ee72bc2502028e8c5346875a4395861d2c8e
Student <example@example.com>
2019-11-20 15:10:43 -0800

    add async version all in one

--- src/main.js
+++ src/main.js
@@ -2,3 +2,22 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
+
+
+$(document).ready(function() {
+  $('#btn').click(function() {
+
+    fetch(`https://cat-fact.herokuapp.com/facts`)
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(jsonifiedResponse) {
+        getElements(jsonifiedResponse);
+      });
+
+   const getElements = function(response) {
+     let i = Math.floor(Math.random() * Math.floor(50));
+      $('.showCatFact').text(`${response.all[i].text}`);
+    }
+  });
+});
